cd2fe69c845ebcd00a6ed519365f7b8221bc93ed
Tomoooji <tomoji.k.2nd@gmail.com>
2026-04-09 10:49:35 +0900

    .

--- LEGO_Arm/LEGO_Arm.ino
+++ LEGO_Arm/LEGO_Arm.ino
@@ -9,7 +9,7 @@ int servoR_ang, servoL_ang;
 servo ServoR, ServoL;
 
 const int CENTER_X = 512, CENTER_Y = 512;
-const speed; //12bit to hand potition
+const speed_coeff; //12bit to hand potition move
 
 // control from arduino uno shield
 const int PIN_JSTK_X = A0;
@@ -17,6 +17,11 @@ const int PIN_JSTK_Y = A1;
 const int PIN_SERVO_X = 6;
 const int PIN_SERVO_Y = 5;
 
+void calcAng(){
+  
+}
+
 void setup(){}
 
 void loop(){}
+
